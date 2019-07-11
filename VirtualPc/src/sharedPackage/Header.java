a17ddf7b8d6d81f371e98c067c6c7c8ad24b5ae5
maria <maria-afara5@hotmail.com>
2019-07-11 19:17:11 +0300

    some edit

--- VirtualPc/src/sharedPackage/Header.java
+++ VirtualPc/src/sharedPackage/Header.java
@@ -15,13 +15,13 @@ import java.security.NoSuchAlgorithmException;
 import javax.xml.bind.DatatypeConverter;
 
 public class Header implements Serializable, Cloneable {
-    
+
     String Version;
     long DHTL; //set it later, Length of the header
     String TypeOfService;
     int totalLength;  //length of the whole packet
     int Identification = -1;
-    
+
     Boolean moreFragment;
     public int TTL; //TIME TO LIVE
     int Protocol; // the protocol of next level tcp or udp
@@ -31,7 +31,7 @@ public class Header implements Serializable, Cloneable {
     //int portSource;
     // int portDestination;
     String destinationHostname, sourceHostname;
-    
+
     public Header(int Identification, int TTL, InetAddress sourceAddress, InetAddress destinationAddress, String sourceHostname, String destinationHostname) {//, int portSource, int portDestination
         this.Identification = Identification;
         Version = "4+";
@@ -40,37 +40,37 @@ public class Header implements Serializable, Cloneable {
         this.TTL = TTL;
         this.sourceAddress = sourceAddress;
         this.destinationAddress = destinationAddress;
-      //  this.portSource = portSource;
+        //  this.portSource = portSource;
         this.destinationHostname = destinationHostname;
         this.sourceHostname = sourceHostname;
         //  this.portDestination = portDestination;
 
     }
-    
+
     public RoutingTableKey getDestination() {
         return new RoutingTableKey(destinationAddress, destinationHostname);
     }
-    
+
     public String getVersion() {
         return Version;
     }
-    
+
     public long getDHTL() {
         return DHTL;
     }
-    
+
     public String getTypeOfService() {
         return TypeOfService;
     }
-    
+
     public int getTotalLength() {
         return totalLength;
     }
-    
+
     public int getIdentification() {
         return Identification;
     }
-    
+
     public String getDestinationHostname() {
         return destinationHostname;
     }
@@ -82,27 +82,27 @@ public class Header implements Serializable, Cloneable {
     public String getSourceHostname() {
         return sourceHostname;
     }
-    
+
     public Boolean getMoreFragment() {
         return moreFragment;
     }
-    
+
     public int getTTL() {
         return TTL;
     }
-    
+
     public int getProtocol() {
         return Protocol;
     }
-    
+
     public String getHeaderCheksum() {
         return headerCheksum;
     }
-    
+
     public InetAddress getSourceAddress() {
         return sourceAddress;
     }
-    
+
     public InetAddress getDestinationAddress() {
         return destinationAddress;
     }
@@ -117,16 +117,16 @@ public class Header implements Serializable, Cloneable {
     public Header clone() throws CloneNotSupportedException {
         return (Header) super.clone();
     }
-    
+
     @Override
     public String toString() {
-        return "Header{" + "Version=" + Version + ", DHTL=" + DHTL + ", TypeOfService=" + TypeOfService + ", totalLength=" + totalLength + ", Identification=" + Identification + ", moreFragment=" + moreFragment + ", TTL=" + TTL + ", Protocol=" + Protocol + ", headerCheksum=" + headerCheksum + ", sourceAddress=" + sourceAddress + ", destinationAddress=" + destinationAddress + ", destinationHostname=" + destinationHostname + '}';
+        return "\n" + "Version=" + Version + ",\n DHTL=" + DHTL + ",\n TypeOfService=" + TypeOfService + ",\n totalLength=" + totalLength + ", \nIdentification=" + Identification + ",\n moreFragment=" + moreFragment + ",\n TTL=" + TTL + ", \nProtocol=" + Protocol + ", \nheaderCheksum=" + headerCheksum + ", \nsourceAddress=" + sourceAddress + ", \ndestinationAddress=" + destinationAddress + ",\n destinationHostname=" + destinationHostname + '\n';
     }
-    
+
     public String cheksumInput() {
-        return "Header{" + " Identification=" + Identification + ", Version=" + Version + ", DHTL=" + DHTL + ", TypeOfService=" + TypeOfService + ", totalLength=" + totalLength + ", Protocol=" + Protocol + ", sourceAddress=" + sourceAddress + ", destinationAddress=" + destinationAddress +", hostnameDestination=" + destinationHostname + '}';
+        return "Header{" + " Identification=" + Identification + ", Version=" + Version + ", DHTL=" + DHTL + ", TypeOfService=" + TypeOfService + ", totalLength=" + totalLength + ", Protocol=" + Protocol + ", sourceAddress=" + sourceAddress + ", destinationAddress=" + destinationAddress + ", hostnameDestination=" + destinationHostname + '}';
     }
-    
+
     public static String getChecksum(Serializable object) throws IOException, NoSuchAlgorithmException {
         ByteArrayOutputStream baos = null;
         ObjectOutputStream oos = null;
